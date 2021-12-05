cab08cc4720fc236e558ed73f9703e7f84f53699
razzorsec <54918791+razzorsec@users.noreply.github.com>
2021-12-05 17:29:16 +0530

    Create SafeOwn.sol

---
+++ AccessControl/SafeOwn.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: MIT
+// @author Razzor https://twitter.com/razzor_tweet
+pragma solidity ^0.8.0;
+
+contract SafeOwn{
+    bool private isRenounced;
+    address private _Owner;
+    address private _pendingOwner;
+    uint256 private _renounceTime;
+    uint256 private _renounceInterval;
+    
+     /**
+     * @dev Emitted when the Ownership is transferred or renounced. AtTime may hold
+     * a future time value, if there exists a _renounceInterval > 0 for renounceOwnership transaction.
+     */
+    event ownershipTransferred(address indexed currentOwner, address indexed newOwner, uint256 indexed AtTime);
+     /**
+     * @dev Emitted when the Ownership is retained by the current Owner.
+     */
+    event ownershipRetained(address indexed currentOwner, uint256 indexed At);
+
+     /**
+     * @notice Initializes the Deployer as the Owner of the contract.
+     * @param renounceInterval time in seconds after which the Owner will be removed.
+     */
+
+    constructor(uint256 renounceInterval){
+        _Owner = msg.sender;
+        _renounceInterval = renounceInterval;
+        emit ownershipTransferred(address(0), _Owner, block.timestamp);
+    }
+     /**
+     * @notice Throws if the caller is not the Owner.
+     */
+
+    modifier onlyOwner(){
+        require(Owner() == msg.sender, "SafeOwn: Caller is the not the Owner");
+        _;
+    }
+
+     /**
+     * @notice Throws if the caller is not the Pending Owner.
+     */
+
+    modifier onlyPendingOwner(){
+        require(_pendingOwner == msg.sender, "SafeOwn: Caller is the not the Pending Owner");
+        _;
+    }
+
+     /**
+     * @notice Returns the current Owner.
+     * @dev returns zero address after renounce time, if the Ownership is renounced.
+     */
+
+    function Owner() public view virtual returns(address){
+        if(block.timestamp >= _renounceTime && isRenounced){
+            return address(0);
+        }
+        else{
+            return _Owner;
+        }
+    }
+     /**
+     * @notice Returns the Pending Owner.
+     */
+
+    function pendingOwner() public view virtual returns(address){
+        return _pendingOwner;
+    }
+
+     /**
+     * @notice Returns the renounce parameters.
+     * @return bool value determining whether Owner has called renounceOwnership or not.
+     * @return Renounce Interval in seconds after which the Ownership will be renounced.
+     * @return Renounce Time at which the Ownership was/will be renounced. 0 if Ownership retains.
+     */
+    function renounceParams() public view virtual returns(bool, uint256, uint256){
+        return (isRenounced, _renounceInterval, _renounceTime);
+    }
+     /**
+     * @notice Owner can propose ownership to a new Owner(newOwner).
+     * @dev Owner can not propose ownership, if it has called renounceOwnership and 
+     * not retained the ownership yet.
+     * @param newOwner address of the new owner to propose ownership to.
+     */
+    function proposeOwnership(address newOwner) public virtual onlyOwner{
+        require(!isRenounced, "SafeOwn: Ownership has been Renounced");
+        require(newOwner != address(0), "SafeOwn: New Owner can not be a Zero Address");
+        _pendingOwner = newOwner;
+    }
+
+     /**
+     * @notice Pending Owner can accept the ownership proposal and become the new Owner.
+     */
+    function acceptOwnership() public virtual onlyPendingOwner{
+        address currentOwner = _Owner;
+        address newOwner = _pendingOwner;
+        _Owner = _pendingOwner;
+        _pendingOwner = address(0);
+        emit ownershipTransferred(currentOwner, newOwner, block.timestamp);
+    }
+
+     /**
+     * @notice Owner can renounce ownership. Owner will be removed from the
+     * contract after _renounceTime. 
+     * @dev Owner will be immediately removed if the _renounceInterval is 0.
+     * @dev Pending Owner will be immediately removed.
+     */
+    function renounceOwnership() public virtual onlyOwner{
+        require(!isRenounced, "SafeOwn: Already Renounced");
+        if(_pendingOwner != address(0)){
+             _pendingOwner = address(0);
+        }
+        _renounceTime = block.timestamp + _renounceInterval;
+        isRenounced = true;
+        emit ownershipTransferred(_Owner, address(0), _renounceTime);
+    }
+
+     /**
+     * @notice Owner can retain its ownership and cancel the renouncing(if initiated
+     * by Owner).
+     */
+
+    function retainOwnership() public virtual onlyOwner{
+        require(isRenounced, "SafeOwn: Already Retained");
+        _renounceTime = 0;
+        isRenounced = false;
+        emit ownershipRetained(_Owner, block.timestamp);
+    }
+
+}
